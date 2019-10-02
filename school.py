d539f3da0b68c2af2926943edc8c5281c617ad22
Andrew <pi_r_round@hotmail.com>
2019-10-02 11:15:16 -0500

    Completed Object Oriented Attributes with Functions Lab.

--- school.py
+++ school.py
@@ -1,1 +1,15 @@
 class School:
+    def __init__(self, name = None):
+        self.name = name
+        self.roster = {}
+    def add_student(self, name = None, grade = None):
+        if str(grade) in self.roster.keys():
+            self.roster[str(grade)].append(name)
+        else:
+            self.roster[str(grade)] = [name]
+    def grade(self, grade):
+        return self.roster[str(grade)]
+    def sort_roster(self):
+        for grade in self.roster.keys():
+            self.roster[str(grade)].sort()
+        return self.roster
